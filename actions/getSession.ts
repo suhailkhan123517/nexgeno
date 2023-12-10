abfd5610a7f737310bdefad785fac040592ed786
Mohammad suhail <skhan282290@gmail.com>
2023-12-10 16:22:45 +0530

    get session fix

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -1,6 +1,5 @@
+import { authOptions } from "@/utils/auth-options";
 import { getServerSession } from "next-auth";
-
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function getSession() {
   return await getServerSession(authOptions);
