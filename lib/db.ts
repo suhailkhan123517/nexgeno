171779a55069aed324aebd1cbe70ad5ee05aa4b7
Mohammad suhail <skhan282290@gmail.com>
2023-12-27 12:51:20 +0530

    some changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,16 @@
+// Add your code using Prisma Client
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-export default client;
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
