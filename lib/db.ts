3b4fda94ac02ff9a3bac5972ead95c2b450c236b
Mohammad suhail <skhan282290@gmail.com>
2024-01-08 09:57:53 +0530

    db code change

--- lib/db.ts
+++ lib/db.ts
@@ -2,13 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 // Development
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
 
-export const db = globalThis.prisma || new PrismaClient();
+// export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
 // Production
-// export const db = new PrismaClient();
+export const db = new PrismaClient();
