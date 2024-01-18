ea045dd444efe851df60ee7e5bd75e5c0a79e16b
Mohammad suhail <skhan282290@gmail.com>
2024-01-18 16:04:09 +0530

    error solve

--- app/(root)/blog/page.tsx
+++ app/(root)/blog/page.tsx
@@ -1,14 +1,40 @@
 import { Metadata } from "next";
 import BlogClient from "./_components/client";
+import { getPosts } from "@/actions/getPosts";
+import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Blogs",
 };
 
-const BlogPage = async () => {
+interface BlogPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const BlogPage = async ({ searchParams }: BlogPageProps) => {
+  const posts = await getPosts({
+    ...searchParams,
+  });
+
   return (
     <>
-      <BlogClient />
+      {/* <BlogClient /> */}
+      {posts.map((item) => (
+        <div className="" key={item.id}>
+          <Link href={`/blog/${item.slug}`}>
+            <Image width={500} height={500} alt="title" src={item.imageUrl!} />
+          </Link>
+          <p>{item.title}</p>
+          <p>{item.description}</p>
+          <p>{item.slug}</p>
+          <p>{item.textEditor}</p>
+          <p>{item.metaTitle}</p>
+        </div>
+      ))}
     </>
   );
 };
